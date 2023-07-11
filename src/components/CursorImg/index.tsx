da7c3517d2160ffbd39aae567e020025d513efae
badahertz52 <badahertz52@gmail.com>
2023-07-11 22:24:18 +0900

    [Fix] Fix error that position isn't changed when mouse move

--- src/components/CursorImg/index.tsx
+++ src/components/CursorImg/index.tsx
@@ -1,16 +1,26 @@
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useEffect, useState } from "react";
 import { CursorPosition } from "../../type";
 type CursorImgProps = {
   selectedCursor: string;
   cursorPosition: CursorPosition;
 };
 function CursorImg({ selectedCursor, cursorPosition }: CursorImgProps) {
-  const style: CSSProperties = {
-    left: cursorPosition.x,
-    top: cursorPosition.y,
-  };
+  const [style, setStyle] = useState<CSSProperties>();
+  useEffect(() => {
+    setStyle({
+      position: "fixed",
+      left: cursorPosition.x,
+      top: cursorPosition.y,
+    });
+  }, [cursorPosition]);
+
   return (
-    <img src={`/images/${selectedCursor}.png`} alt="cursor img" style={style} />
+    <img
+      className="cursor-img"
+      src={`/images/${selectedCursor}.png`}
+      alt="cursor img"
+      style={style}
+    />
   );
 }
 
