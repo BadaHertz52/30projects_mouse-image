15c5b7b0e781cda63424f4c24b9f314becc6cc27
badahertz52 <badahertz52@gmail.com>
2023-07-11 22:25:09 +0900

    Use classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import CursorBtn from "./components/CursorBtn";
 import { cursors } from "./data";
 import { CursorPosition } from "./type";
 
+import classNames from "classnames";
 function App() {
   const [selectedCursor, setSelectedCursor] = useState<string>();
   const [cursorPosition, setCursorPosition] = useState<CursorPosition>({
@@ -12,18 +13,25 @@ function App() {
   });
   useEffect(() => {}, []);
   return (
-    <div className="App">
-      {cursors?.map((c) => (
-        <CursorBtn
-          name={c}
-          selected={c === selectedCursor}
-          setSelectedCursor={setSelectedCursor}
+    <div className={classNames("App", { on: selectedCursor })}>
       {selectedCursor && (
         <CursorImg
           selectedCursor={selectedCursor}
           cursorPosition={cursorPosition}
         />
-      ))}
+      )}
+      <header>버튼을 눌러서 마우스 커서를 바꿔보세요</header>
+      <div className={classNames("btn-group", { on: selectedCursor })}>
+        {cursors?.map((c, i) => (
+          <CursorBtn
+            key={`cursorBtn_${i}`}
+            name={c}
+            selected={c === selectedCursor}
+            setSelectedCursor={setSelectedCursor}
+          />
+        ))}
+      </div>
+
       <div></div>
     </div>
   );
